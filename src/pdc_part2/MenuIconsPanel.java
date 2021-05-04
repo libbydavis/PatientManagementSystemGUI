4bc535ae8d5ac7fbf86ca4e496d962b8f55e73cf
libbydavis <44419483+libbydavis@users.noreply.github.com>
2021-05-04 23:30:31 +1200

    added panels and back buttons
    added prescription and prescription panels and added back buttons to all of them

--- src/pdc_part2/MenuIconsPanel.java
+++ src/pdc_part2/MenuIconsPanel.java
@@ -1,6 +1,5 @@
 package pdc_part2;
 
-import pdc_part2.PatientManagementView;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -15,7 +14,7 @@ public class MenuIconsPanel extends JPanel {
     public MenuIconsPanel(PatientManagementView frame) throws IOException {
         setLayout(new GridLayout(0, 5));
         //see patients
-        BufferedImage patientsIcon = ImageIO.read(new File("src\\Images\\iconPDCPatientsSmall.png"));
+        BufferedImage patientsIcon = ImageIO.read(new File("C:\\Users\\libst\\IdeaProjects\\TestingPart2Code\\src\\iconPDCPatientsSmall.png"));
         JButton patientsButton = new JButton(new ImageIcon(patientsIcon));
         patientsButton.setBorder(BorderFactory.createEmptyBorder());
         patientsButton.setContentAreaFilled(false);
@@ -23,7 +22,7 @@ public class MenuIconsPanel extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Object source = e.getSource();
-                PatientsPanel patientsPanel = new PatientsPanel(getWidth(), getHeight());
+                PatientsPanel patientsPanel = new PatientsPanel(frame, getWidth(), getHeight());
                 frame.remove(store);
                 frame.add(patientsPanel);
                 frame.revalidate();
@@ -32,16 +31,36 @@ public class MenuIconsPanel extends JPanel {
         });
         add(patientsButton);
         //appointments
-        BufferedImage appointmentsIcon = ImageIO.read(new File("src\\Images\\iconPDCAppointmentsSmall.png"));
+        BufferedImage appointmentsIcon = ImageIO.read(new File("C:\\Users\\libst\\IdeaProjects\\TestingPart2Code\\src\\iconPDCAppointmentsSmall.png"));
         JButton appointmentsButton = new JButton(new ImageIcon(appointmentsIcon));
         appointmentsButton.setBorder(BorderFactory.createEmptyBorder());
         appointmentsButton.setContentAreaFilled(false);
+        appointmentsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object source = e.getSource();
+                AppointmentsPanel appointmentsPanel = new AppointmentsPanel(frame);
+                frame.remove(store);
+                frame.add(appointmentsPanel);
+                frame.revalidate();
+            }
+        });
         add(appointmentsButton);
         //prescription
-        BufferedImage prescriptionIcon = ImageIO.read(new File("src\\Images\\iconPDCPrescriptionSmall.png"));
+        BufferedImage prescriptionIcon = ImageIO.read(new File("C:\\Users\\libst\\IdeaProjects\\TestingPart2Code\\src\\iconPDCPrescriptionSmall.png"));
         JButton prescriptionButton = new JButton(new ImageIcon(prescriptionIcon));
         prescriptionButton.setBorder(BorderFactory.createEmptyBorder());
         prescriptionButton.setContentAreaFilled(false);
+        prescriptionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object source = e.getSource();
+                PrescriptionPanel prescriptionPanel = new PrescriptionPanel(frame);
+                frame.remove(store);
+                frame.add(prescriptionPanel);
+                frame.revalidate();
+            }
+        });
         add(prescriptionButton);
     }
 }
